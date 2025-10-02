5260318c8ef317b19e2ed0070f911d3ba57dfe00
Brandon Pham <brandon_pham@shilab.com>
2025-10-01 22:04:27 -0500

    fantasyTeam class definition

--- server/src/core/fantasyTeam/fantasyTeam.ts
+++ server/src/core/fantasyTeam/fantasyTeam.ts
@@ -1,3 +1,58 @@
 export class FantasyTeam {
     // TODO: define roster, owner info, scoring, etc.
+    private ownerId: string;
+
+    private leagueId: string;
+
+    private draftId: string;
+
+    // https://sleepercdn.com/avatars/thumbs/<avatar_id>
+    private avatarId: string;
+
+    // TODO: narrow object type
+    private leagueSettings: Record<any, any>;
+
+    // TODO: confirm object type
+    private scoringSettings: Record<string, number>;
+
+    private rosterPositions: string[];
+
+    private teamStats: Record<string, number>;
+
+    private seasonYear: string;
+
+    private players: string[];
+
+    /** ID of team within league. */
+    private rosterId: number;
+
+    constructor(
+        ownerId: string,
+        leagueId: string,
+        draftId: string,
+        avatarId: string,
+        leagueSettings: Record<any, any>,
+        scoringSettings: Record<string, number>,
+        rosterPositions: string[],
+        teamStats: Record<string, number>,
+        seasonYear: string,
+        players: string[],
+        rosterId: number
+    ) {
+        this.ownerId = ownerId;
+        this.leagueId = leagueId;
+        this.draftId = draftId;
+        this.avatarId = avatarId;
+        this.leagueSettings = leagueSettings;
+        this.scoringSettings = scoringSettings;
+        this.rosterPositions = rosterPositions;
+        this.teamStats = teamStats;
+        this.seasonYear = seasonYear;
+        this.players = players;
+        this.rosterId = rosterId;
+    }
+
+    public getOwnerId(): string {
+        return this.ownerId;
+    }
 }
